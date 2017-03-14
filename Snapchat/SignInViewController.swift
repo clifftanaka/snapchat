758b46d1e6470b23b2f24ac03436e968c338e646
Cliff Tanaka <tanaka.cliff@gmail.com>
2017-03-14 16:18:39 +0900

    add firebase and add signin design

---
+++ Snapchat/SignInViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SignInViewControlle.swift
+//  Snapchat
+//
+//  Created by Cliff Tanaka on 2017/03/14.
+//  Copyright © 2017 kurifu. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func turnUpTapped(_ sender: Any) {
+        
+    }
+
+}
+
